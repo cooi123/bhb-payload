b406f963387495d7b1e525bc9e86ea6b82d76b7c
the-mwbs-builder <auswbs@gmail.com>
2025-11-27 18:40:15 +1100

    fix missing field

--- src/endpoints/seed/home.ts
+++ src/endpoints/seed/home.ts
@@ -145,6 +145,7 @@ export const home: (args: HomeArgs) => RequiredDataFromCollectionSlug<'pages'> =
       {
         blockName: 'Content Block',
         blockType: 'content',
+        backgroundColor:"primary",
         columns: [
           {
             richText: {
