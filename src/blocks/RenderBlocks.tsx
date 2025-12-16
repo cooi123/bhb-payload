a420ff8e14bd98aca783acaccc879949655b0fbf
cooi123 <calebooi@hotmail.com>
2025-12-16 23:49:41 +1100

    Merge branch 'main' of https://github.com/Mount-Waverley-Building-Supply/BHB

--- src/blocks/RenderBlocks.tsx
+++ src/blocks/RenderBlocks.tsx
@@ -5,6 +5,7 @@ import type { Page } from '@/payload-types'
 import { ArchiveBlock } from '@/blocks/ArchiveBlock/Component'
 import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { ContactUsBlock } from '@/blocks/ContactUs/Component'
+import { ProcessTimelineBlock } from '@/blocks/ProcessTimeline/Component'
 import { ContentBlock } from '@/blocks/Content/Component'
 import { FormBlock } from '@/blocks/Form/Component'
 import { MediaBlock } from '@/blocks/MediaBlock/Component'
@@ -18,6 +19,7 @@ const blockComponents = {
   cta: CallToActionBlock,
   formBlock: FormBlock,
   mediaBlock: MediaBlock,
+  processTimeline: ProcessTimelineBlock,
   mediaCarousel: MediaCarouselBlock,
   section: SectionBlock,
 }
