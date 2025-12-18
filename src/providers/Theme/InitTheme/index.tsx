29f1e21a2b535c3cd7dba970414fe8fdd7154b9c
cooi123 <calebooi@hotmail.com>
2025-12-18 18:12:07 +1100

    force to light mode

--- src/providers/Theme/InitTheme/index.tsx
+++ src/providers/Theme/InitTheme/index.tsx
@@ -1,11 +1,20 @@
 import Script from 'next/script'
 import React from 'react'
 
-import { defaultTheme, themeLocalStorageKey } from '../ThemeSelector/types'
-
 export const InitTheme: React.FC = () => {
+  //set theme to light (no dark mode)
   return (
     // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
-   <></>
+    <Script
+      dangerouslySetInnerHTML={{
+        __html: `
+  (function () {
+    document.documentElement.setAttribute('data-theme', 'light')
+  })();
+  `,
+      }}
+      id="theme-script"
+      strategy="beforeInteractive"
+    />
   )
 }
