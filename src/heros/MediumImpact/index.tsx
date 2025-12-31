fff7a3022f4672c32cb1ef6e379d8a3c20e4b176
cooi123 <calebooi@hotmail.com>
2025-12-31 20:41:21 +1100

    remove extra gap

--- src/heros/MediumImpact/index.tsx
+++ src/heros/MediumImpact/index.tsx
@@ -8,9 +8,9 @@ import RichText from '@/components/RichText'
 
 export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   return (
-    <div className="">
-      <div className="container mb-8">
-        {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
+    <div>
+      <div className="container">
+        {richText && <RichText data={richText} enableGutter={false} />}
 
         {Array.isArray(links) && links.length > 0 && (
           <ul className="flex gap-4">
@@ -23,12 +23,11 @@ export const MediumImpactHero: React.FC<Page['hero']> = ({ links, media, richTex
             })}
           </ul>
         )}
-      </div>
-      <div className="container ">
+        
         {media && typeof media === 'object' && (
-          <div>
+          <div >
             <Media
-              className="-mx-4 md:-mx-8 2xl:-mx-16"
+              className=""
               imgClassName=""
               priority
               resource={media}
